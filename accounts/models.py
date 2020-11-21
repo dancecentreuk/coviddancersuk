c9335f5f9d790d954f35e5ed53025e8c60ae76ef
dcuk <dcuk@dcuks-MacBook-Pro-2.local>
2020-11-21 18:48:01 +0000

    adjusted image resize 5

--- accounts/models.py
+++ accounts/models.py
@@ -130,15 +130,18 @@ class Candidate(models.Model):
 
         memfile = BytesIO()
 
-        img = Image.open(self.profile_image)
-        if img.height > 500 or img.width > 500:
-            output_size = (500, 500)
-            img.thumbnail(output_size, Image.ANTIALIAS)
-            img.save(memfile, 'PNG', quality=95)
-            default_storage.save(self.profile_image.name, memfile)
-            memfile.close()
-            img.close()
+        try:
 
+            img = Image.open(self.profile_image)
+            if img.height > 500 or img.width > 500:
+                output_size = (500, 500)
+                img.thumbnail(output_size, Image.ANTIALIAS)
+                img.save(memfile, 'PNG', quality=95)
+                default_storage.save(self.profile_image.name, memfile)
+                memfile.close()
+                img.close()
+        except Image.DoesNotExist:
+            pass
 
 
 
